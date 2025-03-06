4dedc47cfd279de36ccb6e533dd0a8d1ade177d4
ChennaKrishnakdp <144444911+ChennaKrishnakdp@users.noreply.github.com>
2025-03-05 19:22:26 -0500

    Add files via upload

---
+++ Casting.java
@@ -0,0 +1,16 @@
+package BasicsPractice;
+
+public class Casting 
+	{
+		public static void main(String args[])
+		{
+			byte b = 89;
+			int a = 1024;
+			byte res = (byte) a;
+			System.out.println("converterd value of res is " +res);
+			System.out.println(b);	
+			
+			
+			
+		}	
+	}
